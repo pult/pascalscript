0c0638217847246037788e0704699bb18c79970f
Vadim Lou <pult.ukr@gmail.com>
2015-06-26 19:28:02 +0300

    Update uPSC_comobj.pas

--- Source/uPSC_comobj.pas
+++ Source/uPSC_comobj.pas
@@ -21,33 +21,39 @@ implementation
 
 procedure SIRegister_ComObj(cl: TPSPascalCompiler);
 begin
-{$IFDEF FPC}
-    {$IFDEF PS_FPC_HAS_COM}
-    cl.AddTypeS('HResult', 'LongInt');
-    cl.AddTypeS('TGUID', 'record D1: LongWord; D2: Word; D3: Word; D4: array[0..7] of Byte; end;');
-    cl.AddTypeS('TCLSID', 'TGUID');
-    cl.AddTypeS('TIID', 'TGUID');
-    cl.AddDelphiFunction('procedure OleCheck(Result: HResult);');
-    cl.AddDelphiFunction('function StringToGUID(const S: string): TGUID;');
-    cl.AddDelphiFunction('function CreateComObject(const ClassID: TGUID): IUnknown;');
-    cl.AddDelphiFunction('function CreateOleObject(const ClassName: String): IDispatch;');
-    cl.AddDelphiFunction('function GetActiveOleObject(const ClassName: String): IDispatch;');
-    {$ENDIF}
-{$ELSE}
+{+}
   cl.AddTypeS('HResult', 'LongInt');
   cl.AddTypeS('TGUID', 'record D1: LongWord; D2: Word; D3: Word; D4: array[0..7] of Byte; end;');
   cl.AddTypeS('TCLSID', 'TGUID');
   cl.AddTypeS('TIID', 'TGUID');
+{$IFDEF FPC}
+    {$IFDEF PS_FPC_HAS_COM}
+    cl.AddDelphiFunction('procedure OleCheck(Result: HResult);');
+    //
+    cl.AddDelphiFunction('function CreateGUID(var Guid: TGUID): HRESULT;');
+    cl.AddDelphiFunction('function StringToGUID(const S: string): TGUID;');
+    cl.AddDelphiFunction('function GUIDToString(const GUID: TGUID): string;');
+    cl.AddDelphiFunction('function IsEqualGUID(const guid1, guid2: TGUID): Boolean;');
+    //
+    cl.AddDelphiFunction('function CreateComObject(const ClassID: TGUID): IUnknown;');
+    cl.AddDelphiFunction('function CreateOleObject(const ClassName: String): IDispatch;');
+    cl.AddDelphiFunction('function GetActiveOleObject(const ClassName: String): IDispatch;');
+    {$ENDIF}
+{$ELSE !FPC}
   cl.AddDelphiFunction('procedure OleCheck(Result: HResult);');
-{$IFNDEF PS_NOINTERFACES}
-{$IFDEF DELPHI3UP}
+  {$IFNDEF PS_NOINTERFACES}
+  {$IFDEF DELPHI3UP}
+  cl.AddDelphiFunction('function CreateGUID(var Guid: TGUID): HRESULT;');
   cl.AddDelphiFunction('function StringToGUID(const S: string): TGUID;');
+  cl.AddDelphiFunction('function GUIDToString(const GUID: TGUID): string;');
+  cl.AddDelphiFunction('function IsEqualGUID(const guid1, guid2: TGUID): Boolean;');
   cl.AddDelphiFunction('function CreateComObject(const ClassID: TGUID): IUnknown;');
-{$ENDIF}
-{$ENDIF}
+  {$ENDIF}
+  {$ENDIF}
   cl.AddDelphiFunction('function CreateOleObject(const ClassName: String): IDispatch;');
   cl.AddDelphiFunction('function GetActiveOleObject(const ClassName: String): IDispatch;');
-{$ENDIF}
+{$ENDIF !FPC}
+{+.}
 end;
 
 end.

a612471f22d526d2781eb89fb3ac27c02d56932d
Vadim Lou <pult.ukr@gmail.com>
2019-12-21 03:17:43 +0200

    sync with master changes

--- Source/uPSC_buttons.pas
+++ Source/uPSC_buttons.pas
@@ -8,7 +8,7 @@ uses
 {
   Will register files from:
     Buttons
- 
+
   Requires
       STD, classes, controls and graphics and StdCtrls
 }
@@ -23,43 +23,47 @@ implementation
 
 procedure SIRegisterTSPEEDBUTTON(Cl: TPSPascalCompiler);
 begin
-  with Cl.AddClassN(cl.FindClass('TGRAPHICCONTROL'), 'TSPEEDBUTTON') do
+  with Cl.AddClassN(cl.FindClass('TGraphicControl'), 'TSpeedButton') do
   begin
-    RegisterProperty('ALLOWALLUP', 'BOOLEAN', iptrw);
-    RegisterProperty('GROUPINDEX', 'INTEGER', iptrw);
-    RegisterProperty('DOWN', 'BOOLEAN', iptrw);
-    RegisterProperty('CAPTION', 'String', iptrw);
-    RegisterProperty('FONT', 'TFont', iptrw);
-    RegisterProperty('GLYPH', 'TBITMAP', iptrw);
-    RegisterProperty('LAYOUT', 'TBUTTONLAYOUT', iptrw);
-    RegisterProperty('MARGIN', 'INTEGER', iptrw);
-    RegisterProperty('NUMGLYPHS', 'BYTE', iptrw);
-    RegisterProperty('PARENTFONT', 'Boolean', iptrw);
-    RegisterProperty('PARENTSHOWHINT', 'Boolean', iptrw);
-    RegisterProperty('SPACING', 'INTEGER', iptrw);
-    RegisterProperty('ONCLICK', 'TNotifyEvent', iptrw);
-    RegisterProperty('ONDBLCLICK', 'TNotifyEvent', iptrw);
-    RegisterProperty('ONMOUSEDOWN', 'TMouseEvent', iptrw);
-    RegisterProperty('ONMOUSEMOVE', 'TMouseMoveEvent', iptrw);
-    RegisterProperty('ONMOUSEUP', 'TMouseEvent', iptrw);
+    RegisterProperty('AllowAllUp', 'Boolean', iptrw);
+    {$IFDEF DELPHI4UP}
+    RegisterProperty('Anchors', 'TAnchors', iptrw);
+    {$ENDIF}
+    RegisterProperty('GroupIndex', 'Integer', iptrw);
+    RegisterProperty('Down', 'Boolean', iptrw);
+    RegisterProperty('Caption', 'string', iptrw);
+    RegisterProperty('Font', 'TFont', iptrw);
+    RegisterProperty('Glyph', 'TBitmap', iptrw);
+    RegisterProperty('Layout', 'TButtonLayout', iptrw);
+    RegisterProperty('Margin', 'Integer', iptrw);
+    RegisterProperty('NumGlyphs', 'Byte', iptrw);
+    RegisterProperty('ParentFont', 'Boolean', iptrw);
+    RegisterProperty('ParentShowHint', 'Boolean', iptrw);
+    RegisterProperty('Spacing', 'Integer', iptrw);
+    RegisterProperty('OnClick', 'TNotifyEvent', iptrw);
+    RegisterProperty('OnDblClick', 'TNotifyEvent', iptrw);
+    RegisterProperty('OnMouseDown', 'TMouseEvent', iptrw);
+    RegisterProperty('OnMouseMove', 'TMouseMoveEvent', iptrw);
+    RegisterProperty('OnMouseUp', 'TMouseEvent', iptrw);
   end;
 end;
 
 procedure SIRegisterTBITBTN(Cl: TPSPascalCompiler);
 begin
-  with Cl.AddClassN(cl.FindClass('TBUTTON'), 'TBITBTN') do
+  with Cl.AddClassN(cl.FindClass('TButton'), 'TBitBtn') do
   begin
-    RegisterProperty('GLYPH', 'TBITMAP', iptrw);
-    RegisterProperty('KIND', 'TBITBTNKIND', iptrw);
-    RegisterProperty('LAYOUT', 'TBUTTONLAYOUT', iptrw);
-    RegisterProperty('MARGIN', 'INTEGER', iptrw);
-    RegisterProperty('NUMGLYPHS', 'BYTE', iptrw);
-    RegisterProperty('STYLE', 'TBUTTONSTYLE', iptrw);
-    RegisterProperty('SPACING', 'INTEGER', iptrw);
+    {$IFDEF DELPHI4UP}
+    RegisterProperty('Anchors', 'TAnchors', iptrw);
+    {$ENDIF}
+    RegisterProperty('Glyph', 'TBitmap', iptrw);
+    RegisterProperty('Kind', 'TBitBtnKind', iptrw);
+    RegisterProperty('Layout', 'TButtonLayout', iptrw);
+    RegisterProperty('Margin', 'Integer', iptrw);
+    RegisterProperty('NumGlyphs', 'Byte', iptrw);
+    RegisterProperty('Style', 'TButtonStyle', iptrw);
+    RegisterProperty('Spacing', 'Integer', iptrw);
   end;
 end;
-
-
 
 procedure SIRegister_Buttons_TypesAndConsts(Cl: TPSPascalCompiler);
 begin
@@ -67,7 +71,6 @@ begin
   Cl.AddTypeS('TButtonState', '(bsUp, bsDisabled, bsDown, bsExclusive)');
   Cl.AddTypeS('TButtonStyle', '(bsAutoDetect, bsWin31, bsNew)');
   Cl.AddTypeS('TBitBtnKind', '(bkCustom, bkOK, bkCancel, bkHelp, bkYes, bkNo, bkClose, bkAbort, bkRetry, bkIgnore, bkAll)');
-
 end;
 
 procedure SIRegister_Buttons(Cl: TPSPascalCompiler);
@@ -79,9 +82,4 @@ end;
 
 // PS_MINIVCL changes by Martijn Laan (mlaan at wintax _dot_ nl)
 
-
 end.
-
-
-
-

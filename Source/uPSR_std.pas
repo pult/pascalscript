7cf1454c6a5a9e307c27d81bb0507b43c76f96e9
Vadim Lou <pult.ukr@gmail.com>
2019-12-21 04:46:28 +0200

    clear vmt reference when object destroyed

--- Source/uPSR_std.pas
+++ Source/uPSR_std.pas
@@ -1,10 +1,8 @@
-
 unit uPSR_std;
 {$I PascalScript.inc}
 interface
 uses
   uPSRuntime, uPSUtils;
-
 
 procedure RIRegisterTObject(CL: TPSRuntimeClassImporter);
 procedure RIRegisterTPersistent(Cl: TPSRuntimeClassImporter);
@@ -15,14 +13,61 @@ implementation
 uses
   Classes;
 
+{+}
+{.$IFDEF DELPHI} // @@@ TODO: FPC Check ...
+procedure TObject_Free(ASelf: TObject);
+begin
+  if (ASelf <> nil) {my fix for "system.pas":}and (ASelf.ClassType <> nil) then
+  begin
+    ASelf.Destroy;
 
+    //
+    // Clear Self ClssType pointer ( ASelf.ClassType := nil )
+    //
+    //  Effects:
+    //    -  !!! disable call of any vmt methods for destroyed object
+    //    - Double call of ".Free" no generate Exception "EInvalidPointer"
+    //
+    PPointer(ASelf)^ := nil; // !!! disable call of any vmt methods for destroyed object
+    //
+    // Sample:
+    //
+    {
+// ------------------------
+var
+  obj: TObject;
+begin
+  obj := TObject.Create;
+  obj.Free;
+  obj.Free; // default: AV; now: like as nil !
+end.
+// ------------------------
+    }
+    //
+  end;
+end;
+{.$ENDIF DELPHI}
+
+function TObject_ClassName(ASelf: TObject): string;
+begin
+  Result := ASelf.ClassName;
+end;
+{+.}
 
 procedure RIRegisterTObject(CL: TPSRuntimeClassImporter);
 begin
   with cl.Add(TObject) do
   begin
     RegisterConstructor(@TObject.Create, 'Create');
-    RegisterMethod(@TObject.Free, 'Free');
+  {+}
+    {.$IFDEF DELPHI}
+    RegisterMethod(@TObject_Free, 'Free'); // @@@ TODO: FPC Check ...
+    {.$ELSE}
+    //RegisterMethod(@TObject.Free, 'Free');
+    {.$ENDIF}
+
+    RegisterMethod(@TObject_ClassName, 'ClassName'); // class function allow call only for assigned object
+  {+.}
   end;
 end;
 
@@ -36,7 +81,6 @@ end;
 
 procedure TComponentOwnerR(Self: TComponent; var T: TComponent); begin T := Self.Owner; end;
 
-
 procedure TCOMPONENTCOMPONENTS_R(Self: TCOMPONENT; var T: TCOMPONENT; t1: INTEGER); begin T := Self.COMPONENTS[t1]; end;
 procedure TCOMPONENTCOMPONENTCOUNT_R(Self: TCOMPONENT; var T: INTEGER); begin t := Self.COMPONENTCOUNT; end;
 procedure TCOMPONENTCOMPONENTINDEX_R(Self: TCOMPONENT; var T: INTEGER); begin t := Self.COMPONENTINDEX; end;
@@ -44,7 +88,6 @@ procedure TCOMPONENTCOMPONENTINDEX_W(Self: TCOMPONENT; T: INTEGER); begin Self.C
 procedure TCOMPONENTCOMPONENTSTATE_R(Self: TCOMPONENT; var T: TCOMPONENTSTATE); begin t := Self.COMPONENTSTATE; end;
 procedure TCOMPONENTDESIGNINFO_R(Self: TCOMPONENT; var T: LONGINT); begin t := Self.DESIGNINFO; end;
 procedure TCOMPONENTDESIGNINFO_W(Self: TCOMPONENT; T: LONGINT); begin Self.DESIGNINFO := t; end;
-
 
 procedure RIRegisterTComponent(Cl: TPSRuntimeClassImporter);
 begin
@@ -62,12 +105,6 @@ begin
     RegisterPropertyHelper(@TCOMPONENTDESIGNINFO_R, @TCOMPONENTDESIGNINFO_W, 'DesignInfo');
   end;
 end;
-
-
-
-
-
-
 
 procedure RIRegister_Std(Cl: TPSRuntimeClassImporter);
 begin

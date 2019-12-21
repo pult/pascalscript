e27d2343b3cc328dfb2d2c0031c9e08d601c4466
Vadim Lou <pult.ukr@gmail.com>
2019-12-21 05:12:40 +0200

    Update imp.dpr

--- unit-importing/imp.dpr
+++ unit-importing/imp.dpr
@@ -1,7 +1,19 @@
 program imp;
-
+{+}
+{$i app_linker.inc}
+{$i FxtVer.inc}
+{+.}
 uses
   Forms,
+  {+}
+  //ExceptDlg,
+  //ExceptDlg in '..\..\..\JCL\VCL\ExceptDlg.pas' {ExceptionDialog},
+  {$IFDEF CONDITIONALEXPRESSIONS}
+  {$IF CompilerVersion <= 18.50} // Delphi 2007 Down
+    VCLFixPack, { optional }
+  {$IFEND}
+  {$ENDIF}
+  {+.}
   Main in 'Main.pas' {frmMain},
   ParserU in 'ParserU.pas',
   ParserUtils in 'ParserUtils.pas',
@@ -10,9 +22,14 @@ uses
   UFrmGotoLine in 'UFrmGotoLine.pas' {frmGotoLine};
 
 {$R *.res}
-
+{+}
+{.$R Main.dfm}
+{+.}
 begin
   Application.Initialize;
+  {+}
+  Application.ShowMainForm := True;
+  {+.}
   Application.CreateForm(TfrmMain, frmMain);
   Application.CreateForm(TfrmGotoLine, frmGotoLine);
   Application.Run;

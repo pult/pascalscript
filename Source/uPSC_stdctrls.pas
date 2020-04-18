8df433c5443b0042f5c72bae9a7435d3f780a4f6
pult <pult@ukr.net>
2020-04-18 03:21:31 +0300

    Fix master issue 227
    https://github.com/remobjects/pascalscript/issues/227

--- Source/uPSC_stdctrls.pas
+++ Source/uPSC_stdctrls.pas
@@ -557,20 +557,22 @@ end;
 
 procedure SIRegister_StdCtrls_TypesAndConsts(cl: TPSPascalCompiler);
 begin
+  {+}
+  // types:
   cl.AddTypeS('TEditCharCase', '(ecNormal, ecUpperCase, ecLowerCase)');
   cl.AddTypeS('TScrollStyle', '(ssNone, ssHorizontal, ssVertical, ssBoth)');
   cl.AddTypeS('TComboBoxStyle', '(csDropDown, csSimple, csDropDownList, csOwnerDrawFixed, csOwnerDrawVariable)');
-  cl.AddTypeS('TDrawItemEvent', 'procedure(Control: TWinControl; Index: Integer; Rect: TRect; State: Byte)');
-  cl.AddTypeS('TMeasureItemEvent', 'procedure(Control: TWinControl; Index: Integer; var Height: Integer)');
   cl.AddTypeS('TCheckBoxState', '(cbUnchecked, cbChecked, cbGrayed)');
   cl.AddTypeS('TListBoxStyle', '(lbStandard, lbOwnerDrawFixed, lbOwnerDrawVariable)');
   cl.AddTypeS('TScrollCode', '(scLineUp, scLineDown, scPageUp, scPageDown, scPosition, scTrack, scTop, scBottom, scEndScroll)');
-  cl.AddTypeS('TScrollEvent', 'procedure(Sender: TObject; ScrollCode: TScrollCode; var ScrollPos: Integer)');
-
   Cl.addTypeS('TEOwnerDrawState', '(odSelected, odGrayed, odDisabled, odChecked, odFocused, odDefault, odHotLight, odInactive, odNoAccel, odNoFocusRect, odReserved1, odReserved2, odComboBoxEdit)');
-
   cl.AddTypeS('TTextLayout', '(tlTop, tlCenter, tlBottom)');
   cl.AddTypeS('TOwnerDrawState', 'set of TEOwnerDrawState');
+  // events:
+  cl.AddTypeS('TDrawItemEvent', 'procedure(Control: TWinControl; Index: Integer; Rect: TRect; State: TOwnerDrawState)');
+  cl.AddTypeS('TMeasureItemEvent', 'procedure(Control: TWinControl; Index: Integer; var Height: Integer)');
+  cl.AddTypeS('TScrollEvent', 'procedure(Sender: TObject; ScrollCode: TScrollCode; var ScrollPos: Integer)');
+  {+.}
 end;
 
 procedure SIRegister_stdctrls(cl: TPSPascalCompiler);
